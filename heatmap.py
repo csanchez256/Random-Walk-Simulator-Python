4ab3732e8f13b33a226605ac9523bf7a92f0619c
Chris <csanchez505@gmail.com>
2017-02-20 02:07:29 -0700

    first commit

---
+++ heatmap.py
@@ -0,0 +1,123 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import random
+
+#a = np.random.random((16, 16))
+
+rows = 10
+columns = 10
+length = rows - 1
+
+#a = np.zeros((10,10))
+a = np.arange(100).reshape(rows,columns)
+
+#a = np.zeros((8,8,3))
+#a += 0.5 # "Black" color. Can also be a sequence of r,g,b with values 0-1.
+#a[::2, ::2] = 1 # "White" color
+#a[1::2, 1::2] = 1 # "White" color
+
+
+#print a.shape
+
+
+		
+
+plt.imshow(a, interpolation='nearest'	)
+
+
+# function that starts walk
+def randomWalk(i,j):
+
+	for itr in range (2):
+		x = random.randint(0, 8)
+		
+		if (x == 0):
+			i = i -1
+			j = j-1
+			if( (i < 0) or (j < 0) or (i > length) or (j > length) ):
+				print "fall"
+			else:
+		        	a[i][j] = 1	
+
+		elif (x == 1):
+			i = i - 1
+			if( i < 0 or j < 0 or i > length or j > length ):
+				print "fall"
+			else:
+				a[i][j] = 1
+
+		elif (x == 2):
+			i = i - 1
+			j = j + 1
+			if( i < 0 or j < 0 or i > length or j > length ):
+				print "fall"
+			else:
+				a[i][j] = 1
+
+		elif (x == 3):
+			j = j - 1
+
+			if( i < 0 or j < 0 or i > length or j > length ):
+				print "fall"
+			else:
+				a[i][j] = 1
+			
+
+		elif (x == 4):
+			if( i < 0 or j < 0 or i > length or j > length ):
+				print "fall"
+			else:
+				a[i][j] = 1
+			
+		elif (x == 5):
+			j = j + 1
+			if( i < 0 or j < 0 or i > length or j > length ):
+				print "fall"
+			else:
+				a[i][j] = 1
+
+		elif (x == 6):
+			i = i + 1
+			j = j - 1
+			if( i < 0 or j < 0 or i > length or j > length ):
+				print "fall"
+			else:
+				a[i][j] = 1
+
+		elif (x == 7):
+			i = i + 1
+			if( i < 0 or j < 0 or i > length or j > length ):
+				print "fall"
+			else:
+				a[i][j] = 1
+
+		elif (x == 8):
+			i = i + 1
+			j = j + 1
+			if( i < 0 or j < 0 or i > length or j > length ):
+				print "fall"
+			else:
+				a[i][j] = 1
+
+
+
+def simulation():
+	for i in range (rows):
+		for j in range (columns):
+			#print random.randint(0, (rows*columns) -1)
+			randomWalk(i,j)
+
+for row in a:
+	print row
+
+#this is to shift axis' ticks to line up with squares
+ax = plt.gca();
+#ax.set_yticks(np.arange(-.5, 10, 1));
+#ax.set_yticklabels(np.arange(1, 11, 1));
+
+#eliminates x ticks
+ax.set(xticks=[] ,yticks=[])
+
+
+plt.show()
+
