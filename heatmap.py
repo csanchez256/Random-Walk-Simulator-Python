242f5a188cfed6f161c491fa7b89f1ab09bb1010
Chris <csanchez505@gmail.com>
2017-02-20 03:03:59 -0700

    second commit

--- heatmap.py
+++ heatmap.py
@@ -2,6 +2,12 @@ import matplotlib.pyplot as plt
 import numpy as np
 import random
 
+#======================
+# This will calculate the expected number of steps
+# to fall off
+#
+#======================
+
 #a = np.random.random((16, 16))
 
 rows = 10
@@ -22,90 +28,112 @@ a = np.arange(100).reshape(rows,columns)
 
 		
 
-plt.imshow(a, interpolation='nearest'	)
+plt.imshow(a, cmap = 'hot'	)
 
 
 # function that starts walk
 def randomWalk(i,j):
-
-	for itr in range (2):
+	counter = 0
+	for itr in range (50):
 		x = random.randint(0, 8)
 		
 		if (x == 0):
-			i = i -1
+			i = i-1
 			j = j-1
+			counter = counter + 1
 			if( (i < 0) or (j < 0) or (i > length) or (j > length) ):
 				print "fall"
+				break
 			else:
 		        	a[i][j] = 1	
 
 		elif (x == 1):
 			i = i - 1
+			counter = counter + 1
 			if( i < 0 or j < 0 or i > length or j > length ):
 				print "fall"
+				break
 			else:
 				a[i][j] = 1
 
 		elif (x == 2):
 			i = i - 1
 			j = j + 1
+			counter = counter + 1
 			if( i < 0 or j < 0 or i > length or j > length ):
 				print "fall"
+				break
 			else:
 				a[i][j] = 1
 
 		elif (x == 3):
 			j = j - 1
-
+			counter = counter + 1
 			if( i < 0 or j < 0 or i > length or j > length ):
 				print "fall"
+				break
 			else:
 				a[i][j] = 1
 			
 
 		elif (x == 4):
+			counter = counter + 1
 			if( i < 0 or j < 0 or i > length or j > length ):
 				print "fall"
+				break
 			else:
 				a[i][j] = 1
 			
 		elif (x == 5):
 			j = j + 1
+			counter = counter + 1
 			if( i < 0 or j < 0 or i > length or j > length ):
 				print "fall"
+				break
 			else:
 				a[i][j] = 1
 
 		elif (x == 6):
 			i = i + 1
 			j = j - 1
+			counter = counter + 1
 			if( i < 0 or j < 0 or i > length or j > length ):
 				print "fall"
+				break
 			else:
 				a[i][j] = 1
 
 		elif (x == 7):
 			i = i + 1
+			counter = counter + 1
 			if( i < 0 or j < 0 or i > length or j > length ):
 				print "fall"
+				break
 			else:
 				a[i][j] = 1
 
 		elif (x == 8):
 			i = i + 1
 			j = j + 1
+			counter = counter + 1
 			if( i < 0 or j < 0 or i > length or j > length ):
 				print "fall"
+				break
 			else:
 				a[i][j] = 1
 
+	print "number of steps is %d" % counter
 
 
+def testTrial():
+	randomWalk(5,8)	
 def simulation():
 	for i in range (rows):
 		for j in range (columns):
 			#print random.randint(0, (rows*columns) -1)
 			randomWalk(i,j)
+testTrial()
+#simulation()
 
 for row in a:
 	print row
