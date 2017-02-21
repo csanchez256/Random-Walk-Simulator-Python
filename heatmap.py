f4de7eaafb25cc7f57599ff68f0500694ab6ad8e
Chris <csanchez505@gmail.com>
2017-02-21 16:35:35 -0700

    Probabilities and bonus question

--- heatmap.py
+++ heatmap.py
@@ -21,11 +21,7 @@ expectedNumTable = np.arange(100).reshape(rows,columns)
 
 bonusMatrix = np.arange(64).reshape(8,8)
 
-#a = np.zeros((8,8,3))
-#a += 0.5 # "Black" color. Can also be a sequence of r,g,b with values 0-1.
-#a[::2, ::2] = 1 # "White" color
-#a[1::2, 1::2] = 1 # "White" color
-
+probabilityTable = np.arange(100).reshape(rows,columns)
 
 #print a.shape
 
@@ -35,7 +31,13 @@ bonusMatrix = np.arange(64).reshape(8,8)
 #plt.imshow(expectedNumTable, cmap = 'hot',interpolation='nearest')
 
 
-plt.imshow(bonusMatrix, cmap = 'hot' ,interpolation='nearest')
+#======================
+# Set first argument to 
+# whatever matrix you
+# WANT TO DISPLAY
+#======================
+
+plt.imshow(expectedNumTable, cmap = 'hot' ,interpolation='nearest')
 
 
 # function that starts walk
@@ -141,10 +143,15 @@ def randomWalk(i,j):
 #=========================== NORTH EDGE =========================================================
 
 # function that starts walk
-def NorthEdge(i,j):
+def NorthEdge(i,j,numTrials):
 	#a = np.arange(100).reshape(rows,columns)
-	counter = 0
-	stepsize = 50
+
+	start_i = i
+	start_j = j
+
+	counter   = 0
+	n_counter = 0
+	stepsize  = 50
 
 	for itr in range (stepsize):
 		x = random.randint(0, 8)
@@ -155,6 +162,7 @@ def NorthEdge(i,j):
 			counter = counter + 1
 			if(i < 0):
 				print "fall"
+				n_counter = n_counter + 1
 				break;
 
 			elif ( (j < 0) or (i > length) or (j > length) ):
@@ -169,6 +177,7 @@ def NorthEdge(i,j):
 
 			if(i < 0):
 				print "fall"
+				n_counter = n_counter + 1
 				break;
 
 			elif ( (j < 0) or (i > length) or (j > length) ):
@@ -184,6 +193,7 @@ def NorthEdge(i,j):
 
 			if(i < 0):
 				print "fall"
+				n_counter = n_counter + 1
 				break;
 
 			elif ( (j < 0) or (i > length) or (j > length) ):
@@ -198,6 +208,7 @@ def NorthEdge(i,j):
 
 			if(i < 0):
 				print "fall"
+				n_counter = n_counter + 1
 				break;
 
 			elif ( (j < 0) or (i > length) or (j > length) ):
@@ -212,6 +223,7 @@ def NorthEdge(i,j):
 
 			if(i < 0):
 				print "fall"
+				n_counter = n_counter + 1
 				break;
 
 			elif ( (j < 0) or (i > length) or (j > length) ):
@@ -226,6 +238,7 @@ def NorthEdge(i,j):
 
 			if(i < 0):
 				print "fall"
+				n_counter = n_counter + 1
 				break;
 
 			elif ( (j < 0) or (i > length) or (j > length) ):
@@ -241,6 +254,7 @@ def NorthEdge(i,j):
 
 			if(i < 0):
 				print "fall"
+				n_counter = n_counter + 1
 				break;
 
 			elif ( (j < 0) or (i > length) or (j > length) ):
@@ -255,6 +269,7 @@ def NorthEdge(i,j):
 
 			if(i < 0):
 				print "fall"
+				n_counter = n_counter + 1
 				break;
 
 			elif ( (j < 0) or (i > length) or (j > length) ):
@@ -270,6 +285,7 @@ def NorthEdge(i,j):
 
 			if(i < 0):
 				print "fall"
+				n_counter = n_counter + 1
 				break;
 
 			elif ( (j < 0) or (i > length) or (j > length) ):
@@ -279,17 +295,25 @@ def NorthEdge(i,j):
 		        	a[i][j] = 1
 
 	#print "number of steps is %d\n" % counter
-	return counter
+	#returns a tuple
+
+	return counter, n_counter
 
 
 
 #====================================================================================
 
+#def probability(i,j,numTrials):
+
+
+
+#====================================================================================
 
 def bonusWalk(i,j):
 	#a = np.arange(100).reshape(rows,columns)
 	counter = 0
-	stepsize = 500
+	stepsize = 5000
+	stop = False
 
 	for itr in range (stepsize):
 		x = random.randint(0, 8)
@@ -300,31 +324,41 @@ def bonusWalk(i,j):
 			counter = counter + 1
 			if (i < 0):
 				i = i+1
+				stop = True
 			if(j < 0):
 				j = j+1
+				stop = True
 			if (i > 7):
 				i = i-1 
+				stop = True
 			if (j > 7):
 				j = j-1
+				stop = True
 		        
-			bonusMatrix[i][j] = 1
-			break
-				
+			if (stop == True): 
+				bonusMatrix[i][j] = counter
+				break
 
 		elif (x == 1):
 			i = i - 1
 			counter = counter + 1
 			if (i < 0):
 				i = i+1
+				stop = True
 			if(j < 0):
 				j = j+1
+				stop = True
 			if (i > 7):
 				i = i-1 
+				stop = True
 			if (j > 7):
 				j = j-1
+				stop = True
 		        
-			bonusMatrix[i][j] = 1
-			break
+			if (stop == True): 
+				bonusMatrix[i][j] = counter
+				break
+			
 
 		elif (x == 2):
 			i = i - 1
@@ -332,60 +366,83 @@ def bonusWalk(i,j):
 			counter = counter + 1
 			if (i < 0):
 				i = i+1
+				stop = True
 			if(j < 0):
 				j = j+1
+				stop = True
 			if (i > 7):
 				i = i-1 
+				stop = True
 			if (j > 7):
 				j = j-1
+				stop = True
 		        
-			bonusMatrix[i][j] = 1
-			break
+			if (stop == True): 
+				bonusMatrix[i][j] = counter
+				break
+			
 
 		elif (x == 3):
 			j = j - 1
 			counter = counter + 1
 			if (i < 0):
 				i = i+1
+				stop = True
 			if(j < 0):
 				j = j+1
+				stop = True
 			if (i > 7):
 				i = i-1 
+				stop = True
 			if (j > 7):
 				j = j-1
+				stop = True
 		        
-			bonusMatrix[i][j] = 1
-			break
+			if (stop == True): 
+				bonusMatrix[i][j] = counter
+				break
 			
 
 		elif (x == 4):
 			counter = counter + 1
 			if (i < 0):
 				i = i+1
+				stop = True
 			if(j < 0):
 				j = j+1
+				stop = True
 			if (i > 7):
 				i = i-1 
+				stop = True
 			if (j > 7):
 				j = j-1
+				stop = True
 		        
-			bonusMatrix[i][j] = 1
-			break
+			if (stop == True): 
+				bonusMatrix[i][j] = counter
+				break
+			
 			
 		elif (x == 5):
 			j = j + 1
 			counter = counter + 1
 			if (i < 0):
 				i = i+1
+				stop = True
 			if(j < 0):
 				j = j+1
+				stop = True
 			if (i > 7):
 				i = i-1 
+				stop = True
 			if (j > 7):
 				j = j-1
+				stop = True
 		        
-			bonusMatrix[i][j] = 1
-			break
+			if (stop == True): 
+				bonusMatrix[i][j] = counter
+				break
+			
 
 		elif (x == 6):
 			i = i + 1
@@ -393,30 +450,42 @@ def bonusWalk(i,j):
 			counter = counter + 1
 			if (i < 0):
 				i = i+1
+				stop = True
 			if(j < 0):
 				j = j+1
+				stop = True
 			if (i > 7):
 				i = i-1 
+				stop = True
 			if (j > 7):
 				j = j-1
+				stop = True
 		        
-			bonusMatrix[i][j] = 1
-			break
+			if (stop == True): 
+				bonusMatrix[i][j] = counter
+				break
+			
 
 		elif (x == 7):
 			i = i + 1
 			counter = counter + 1
 			if (i < 0):
 				i = i+1
+				stop = True
 			if(j < 0):
 				j = j+1
+				stop = True
 			if (i > 7):
 				i = i-1 
+				stop = True
 			if (j > 7):
 				j = j-1
+				stop = True
 		        
-			bonusMatrix[i][j] = 1
-			break
+			if (stop == True): 
+				bonusMatrix[i][j] = counter
+				break
+			
 
 		elif (x == 8):
 			i = i + 1
@@ -424,34 +493,33 @@ def bonusWalk(i,j):
 			counter = counter + 1
 			if (i < 0):
 				i = i+1
+				stop = True
 			if(j < 0):
 				j = j+1
+				stop = True
 			if (i > 7):
 				i = i-1 
+				stop = True
 			if (j > 7):
 				j = j-1
+				stop = True
 		        
-			bonusMatrix[i][j] = 1
-			break
-
-	#print "number of steps is %d\n" % counter
-	#return counter
-
-
+			if (stop == True): 
+				bonusMatrix[i][j] = counter
+				break
+			
 
 # Just a test function for hard coded values
 def bonus():
 	print 'index of bonus matrix %d\n' % bonusMatrix[2][4]
 	#bonusMatrix[2][4] = bonusWalk(2,4)
 
-	trials = 500
+	trials = 500000
 	for itr in range (trials):
 		bonusWalk(2,4)
 
 	for row in bonusMatrix:
 		print row
-
-
 
 
 # This function is the driver. It iterates every index in the board.	
@@ -461,13 +529,13 @@ def simulation():
 
 			average = runMultipleSimulations(i,j)
 			
-			print 'index: %d, number of steps: %d\n' % (expectedNumTable[i][j] , average)
+			#print 'index: %d, number of steps: %d\n' % (expectedNumTable[i][j] , average)
 
-			expectedNumTable[i][j] = average
+			expectedNumTable[i][j] = average[0]
+			probabilityTable[i][j] = average[1] 
 
 	for row in expectedNumTable:
 		print row
-
 
 
 # this function will run several times for each index to get an average
@@ -475,31 +543,37 @@ def simulation():
 def runMultipleSimulations(i, j):
 
 	averages = []
-	numTrials = 100
+	probabilities = []
+ 	numTrials = 500
 
 	print "i and j: %s %s" % (i,j)
 
 	for itr in range(numTrials):
 
-		a = randomWalk(i,j) 
+		b = randomWalk(i,j) 
 
-		#a = NorthEdge(i,j)
+		a = NorthEdge(i,j,numTrials)
 			
-		averages.append(a) #store all the expected values in an array, then compute the average
-	
+		averages.append(b) #store all the expected values in an array, then compute the average
+		probabilities.append(a[1])
+
+	 
 	average = ( sum( int (n) for n in averages ) ) / numTrials
+	prob = (sum (float (n) for n in probabilities )) 
+
+	print 'probabilites is %d' % prob
 
 	print "average is %s" % average
 
-	return average
+	return average, prob
 
 #==========================================================================================
 
 
 #testTrial()
-#simulation()
+simulation()
 
-bonus()
+#bonus()
 
 #this is to shift axis' ticks to line up with squares
 ax = plt.gca();
